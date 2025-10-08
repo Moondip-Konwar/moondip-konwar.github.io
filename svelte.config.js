f4fe7fec9eeb432f51a7fd1be30188b82d29297d
Moondip Konwar <moondipkonwar9@gmail.com>
2025-10-08 22:57:42 +0530

    Updated for github deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      // default options: pages/build/assets inside 'build' folder
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: process.env.GITHUB_PAGES ? '/moondip-konwar.github.io' : '',
+    }
+  }
 };
 
 export default config;
